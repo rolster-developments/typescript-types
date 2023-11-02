77e605f53f5987cc50ec3bd859ef8009c71c1fa9
ing.dacastillop <ing.dacastillop@gmail.com>
2023-11-02 06:33:26 -0500

    :sparkles: Implement new types for Objects State

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,20 @@ declare global {
   type Undefined<T> = T | undefined;
   type Nulleable<T> = T | null;
   type Unknown<T> = T | unknown;
+
+  type Any<T extends Object> = string | number | symbol | boolean | T;
+
+  type UndefinedObject<T> = {
+    [K in keyof T]: Undefined<T[K]>;
+  };
+
+  type NullableObject<T> = {
+    [K in keyof T]: Nullable<T[K]>;
+  };
+
+  type UnknownObject<T> = {
+    [K in keyof T]: Unknown<T[K]>;
+  };
 }
 
 export {};
